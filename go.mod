49d07c1ea102ea7588b0cec9e4b610113d38dbac
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-06-11 17:25:57 +0200

    Refactor to use Postgres DB

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/theodore-s-beers/qabz
 
 go 1.20
 
-require github.com/go-chi/chi/v5 v5.0.8
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/jackc/pgx/v5 v5.3.1
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,75 +1,92 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
-	"sync"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	_, err = conn.Exec(context.Background(), `
+		CREATE TABLE IF NOT EXISTS qabz_bast (
+			lang TEXT PRIMARY KEY,
+			status TEXT NOT NULL
+		);
+		INSERT INTO qabz_bast (lang, status) VALUES ('en', 'qabż') ON CONFLICT (lang) DO NOTHING
+	`)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create table: %v\n", err)
+		os.Exit(1)
+	}
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	status := &SafeStatus{en: "qabż", fa: "قبض"}
-
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello"))
 	})
 
-	r.Get("/fa", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(status.Get("fa")))
+	r.Get("/en", func(w http.ResponseWriter, r *http.Request) {
+		var status string
+		err := conn.QueryRow(context.Background(), "SELECT status FROM qabz_bast WHERE lang = 'en'").Scan(&status)
+		if err != nil {
+			http.Error(w, "Failed to retrieve status", 500)
+			return
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Write([]byte(status))
 	})
 
-	r.Get("/en", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Write([]byte(status.Get("en")))
+	r.Get("/fa", func(w http.ResponseWriter, r *http.Request) {
+		var status string
+		err := conn.QueryRow(context.Background(), "SELECT status FROM qabz_bast WHERE lang = 'en'").Scan(&status)
+		if err != nil {
+			http.Error(w, "Failed to retrieve status", 500)
+			return
+		}
+
+		if status == "qabż" {
+			status = "قبض"
+		} else {
+			status = "بسط"
+		}
+
+		w.Write([]byte(status))
 	})
 
 	r.With(BasicAuth).Post("/poke", func(w http.ResponseWriter, r *http.Request) {
-		status.Toggle()
-		w.Write([]byte(status.Get("en")))
+		_, err := conn.Exec(context.Background(), "UPDATE qabz_bast SET status = (CASE WHEN status = 'qabż' THEN 'basṭ' ELSE 'qabż' END) WHERE lang = 'en'")
+		if err != nil {
+			http.Error(w, "Failed to update status", 500)
+			return
+		}
+
+		var status string
+		err = conn.QueryRow(context.Background(), "SELECT status FROM qabz_bast WHERE lang = 'en'").Scan(&status)
+		if err != nil {
+			http.Error(w, "Failed to retrieve status", 500)
+			return
+		}
+
+		w.Write([]byte(status))
 	})
 
 	http.ListenAndServe(":8080", r)
-}
-
-//
-// Status type and methods
-//
-
-type SafeStatus struct {
-	mux sync.RWMutex
-	en  string
-	fa  string
-}
-
-func (s *SafeStatus) Get(lang string) string {
-	s.mux.RLock()
-	defer s.mux.RUnlock()
-
-	if lang == "fa" {
-		return s.fa
-	} else {
-		return s.en
-	}
-}
-
-func (s *SafeStatus) Toggle() {
-	s.mux.Lock()
-	defer s.mux.Unlock()
-
-	if s.en == "qabż" {
-		s.en = "basṭ"
-		s.fa = "بسط"
-	} else {
-		s.en = "qabż"
-		s.fa = "قبض"
-	}
 }
 
 //
